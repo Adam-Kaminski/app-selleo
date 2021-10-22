c6c5157cc2805c415313f389b89bf44f6f8233f5
Adam Jachymek <jachymek@outlook.com>
2021-10-22 15:55:19 +0200

    Add Routes & Dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./components/Login";
+import Routes from "./Routes";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,35 +5,41 @@ import Button from "@mui/material/Button";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import { useState } from "react";
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
   const [username, setUsername] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    localStorage.setItem("username", username);
+    console.log("login");
+    handleLogin(username);
   };
-
-  console.log(username);
 
   return (
     <>
       <img className="logo" src={logo} />
-      <Box component="form" noValidate autoComplete="off">
-        <form onSubmit={handleSubmit}>
-          <FormControl sx={{ width: "25ch" }}>
-            <OutlinedInput
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Nazwa użytkownika"
-            />
-            <Button
-              sx={{ marginTop: "10px" }}
-              type="submit"
-              variant="contained"
-              color="primary"
-            >
-              Loguj
-            </Button>
-          </FormControl>
-        </form>
+      <Box
+        onSubmit={handleSubmit}
+        component="form"
+        noValidate
+        autoComplete="off"
+      >
+        <FormControl sx={{ width: "25ch" }}>
+          <OutlinedInput
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            placeholder="Nazwa użytkownika"
+          />
+          <Button
+            sx={{ marginTop: "10px" }}
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={!username}
+          >
+            Loguj
+          </Button>
+        </FormControl>
       </Box>
     </>
   );
