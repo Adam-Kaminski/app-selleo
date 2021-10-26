4d478771b96913b2237a86b5487eae4eb5ce9368
Adam Jachymek <jachymek@outlook.com>
2021-10-26 15:38:59 +0200

    show info about single bundle, added button for adding new bundles, small bundles fixes

--- src/pages/Bundles/Bundles.js
+++ src/pages/Bundles/Bundles.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import '../Bundle/Bundle.scss';
-import { Box } from '@mui/system';
+import './Bundles.scss';
+import getAllTagBundles from '../../queries/getAllTagBundles';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Link } from 'react-router-dom';
-import getAllTagBundles from '../../queries/getAllTagBundles';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
 const Bundle = () => {
   const { data, loading, error } = getAllTagBundles();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   if (loading)
     return (
@@ -16,20 +24,64 @@ const Bundle = () => {
     );
   if (error) return <div>error</div>;
 
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
     <>
-      <ul className="bundle-list">
-        {data.map((singleTagBundle) => {
-          return (
-            <Link key={singleTagBundle._id} to={`/dashboard/bundle/${singleTagBundle._id}`}>
-              <li>
-                {singleTagBundle.name}
-                <span>{singleTagBundle.description}</span>
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} component="form">
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add new bundle
+          </Typography>
+          <OutlinedInput id="modal-modal-description" sx={{ mt: 2, width: '100%' }}></OutlinedInput>
+          <Button
+            sx={{ width: '100%', display: 'block', margin: '10px 0' }}
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Add
+          </Button>
+        </Box>
+      </Modal>
+      <div className="bundles">
+        <ul className="bundles__list">
+          <Button
+            onClick={handleOpen}
+            sx={{ width: '100%', display: 'block', margin: '10px 0' }}
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Add
+          </Button>
+          {data.map((singleTagBundle) => {
+            return (
+              <Link key={singleTagBundle._id} to={`/dashboard/bundle/${singleTagBundle._id}`}>
+                <li>
+                  {singleTagBundle.name}
+                  <span>{singleTagBundle.description}</span>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
