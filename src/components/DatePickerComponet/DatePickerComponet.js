0f256c76f0a5b80e37921d110983143a28331a5f
devmiczal <devmiczal@gmail.com>
2021-10-27 07:37:04 +0200

    Calendar page -> arrow date sellecion, select today

--- src/components/DatePickerComponet/DatePickerComponet.js
+++ src/components/DatePickerComponet/DatePickerComponet.js
@@ -7,79 +7,99 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './DatePickerComponet.scss';
 import { useEffect, useState } from 'react';
 
-const DatePickerComponet = () => {
-  const today = new Date();
-  const [choosenDayDate, setChoosenDayDate] = useState(today);
-  const [choosenDayString, setChoosenDayString] = useState('');
+function getDateString(date) {
+  const dateDayNumber = date.getDate();
+  const dateMonthNumber = date.getMonth() + 1;
+  const dateYearNumber = date.getFullYear();
+  let day = '';
+  let month = '';
+  dateDayNumber < 10 ? (day = '0') : (day = '');
+  dateMonthNumber < 10 ? (month = '0') : (month = '');
+  const dateString = `${day}${dateDayNumber}.${month}${dateMonthNumber}.${dateYearNumber}`;
+  return dateString;
+}
 
-  function getDayFromState() {
-    const dateDayNumber = choosenDayDate.getDate();
-    const dateMonthNumber = choosenDayDate.getMonth() + 1;
-    const dateYearNumber = choosenDayDate.getFullYear();
-    let day = '';
-    let month = '';
-    dateDayNumber < 10 ? (day = '0') : (day = '');
-    dateMonthNumber < 10 ? (month = '0') : (month = '');
-    const dateString = `${day}${dateDayNumber}.${month}${dateMonthNumber}.${dateYearNumber}`;
-    return dateString;
-  }
+const today = new Date();
+const todayState = new Date(today.getTime() - 100);
+const todayString = getDateString(today);
+
+const DatePickerComponet = () => {
+  const [choosenDayDate, setChoosenDayDate] = useState(new Date(todayState.getTime()));
+  const [choosenDayString, setChoosenDayString] = useState('');
 
   function nextDay() {
     let currentDay = choosenDayDate.getDate();
     const currentMonth = choosenDayDate.getMonth();
     const currentYear = choosenDayDate.getFullYear();
+    const newDate = new Date(choosenDayDate.getTime());
     currentDay++;
-    const newDate = new Date();
-    newDate.setDate(currentDay + 1);
+    newDate.setDate(currentDay);
     newDate.setMonth(currentMonth);
     newDate.setFullYear(currentYear);
     if (newDate.getDate() < currentDay) {
       newDate.setMonth(currentMonth + 1);
     }
+    if (today.getTime() > newDate.getTime()) {
+      setChoosenDayDate(newDate);
+    }
+  }
+
+  function prevDay() {
+    let currentDay = choosenDayDate.getDate();
+    const currentMonth = choosenDayDate.getMonth();
+    const currentYear = choosenDayDate.getFullYear();
+    currentDay--;
+    const newDate = new Date(choosenDayDate.getTime());
+    newDate.setDate(currentDay);
+    newDate.setMonth(currentMonth);
+    newDate.setFullYear(currentYear);
+    if (newDate.getDate() > currentDay) {
+      newDate.setMonth(currentMonth - 1);
+    }
     setChoosenDayDate(newDate);
   }
 
   useEffect(() => {
-    const newDateString = getDayFromState();
+    const newDateString = getDateString(choosenDayDate);
     setChoosenDayString(newDateString);
   }, [choosenDayDate]);
 
   return (
-    <>
-      <h1>Calendar</h1>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+      }}
+      className="datePickerMainContainer"
+    >
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'row',
           flexWrap: 'wrap',
-          justifyContent: 'space-around',
+          justifyContent: 'center',
           alignItems: 'center',
         }}
-        className="datePickerMainContainer"
+        className="changeDay"
       >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          className="changeDay"
-        >
-          <Button variant="contained">
-            <ArrowBackIcon />
-          </Button>
-          <h1>{choosenDayString}</h1>
-          <Button variant="contained" onClick={() => nextDay()}>
-            <ArrowForwardIcon />
-          </Button>
-        </Box>
-        <Button variant="contained">
-          <TodayIcon />
+        <Button variant="contained" onClick={() => prevDay()}>
+          <ArrowBackIcon />
+        </Button>
+        <h1>{choosenDayString}</h1>
+        <Button variant="contained" onClick={() => nextDay()}>
+          <ArrowForwardIcon />
         </Button>
       </Box>
-    </>
+      <Button variant="contained" onClick={() => setChoosenDayDate(new Date(todayState.getTime()))}>
+        <h2>Dziś: {todayString}</h2>
+      </Button>
+      <Button variant="contained">
+        <TodayIcon />
+      </Button>
+    </Box>
   );
 };
 
