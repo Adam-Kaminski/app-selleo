9a1a257118c3c0c5a599d0526ab0691d55564d77
Adam Jachymek <jachymek@outlook.com>
2021-10-28 01:43:42 +0200

    styling - settings page

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Settings.scss';
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
@@ -10,19 +10,27 @@ const Settings = () => {
   const { data } = getAllTagBundles();
 
   return (
-    <>
-      <FormGroup>
-        {data?.map((singleTagBundle) => {
-          return (
-            <FormControlLabel
-              key={singleTagBundle._id}
-              control={<Checkbox defaultChecked />}
-              label={singleTagBundle.name}
-            />
-          );
-        })}
-      </FormGroup>
-    </>
+    <div className="settings">
+      <div className="settings__user">
+        <h3>Nazwa użytkownika:</h3>
+        <h3>{localStorage.getItem('username')}</h3>
+      </div>
+      <div className="settings__bundles">
+        <h3>Użytkowane Bundle:</h3>
+        <FormGroup>
+          {data?.map((singleTagBundle) => {
+            return (
+              <FormControlLabel
+                className="settings__checkbox"
+                key={singleTagBundle._id}
+                control={<Checkbox defaultChecked />}
+                label={singleTagBundle.name}
+              />
+            );
+          })}
+        </FormGroup>
+      </div>
+    </div>
   );
 };
 
